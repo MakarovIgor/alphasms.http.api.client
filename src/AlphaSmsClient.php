2b9336f6672f218583971c69a0a5700e5338b221
Makarov Igor <igormakarov1991@gmail.com>
2022-06-04 16:03:51 +0300

    add files

---
+++ src/AlphaSmsClient.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace alphasms\api;
+
+class AlphaSmsClient
+{
+    public function __construct()
+    {
+
+    }
+
+    public function sendSms()
+    {
+        echo 'send';
+    }
+}
